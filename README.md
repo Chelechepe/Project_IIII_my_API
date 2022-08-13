4d4f7ec9882c01a9785ead7eb044f5179cb18379
Edgard Cuadra <ejcuad@gmail.com>
2022-08-13 05:49:22 -0600

    Fixed the SQL connection Issue, created the friends DB

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# Project_IV_my_API
+#
+
+# Project IV My API
+## API - SQL query for Friends the show
+### By: Edgard Cuadra
+### Date: August 13st 2022
+
+#
+
+![local_picture](./images/friends_logo.gif)
--- main.py
+++ main.py
@@ -2,35 +2,28 @@ from flask import Flask
 import random
 from pymongo import MongoClient
 import pymongo
-import tools.mongotools as mongo
+import tools.sqltools as sql
 from flask import jsonify
+from os import name
+from flask import Flask, request, jsonify
+import markdown.extensions.fenced_code
+import pandas as pd
+import json
+import random
+import googletrans
 
 app =Flask(__name__)
 
-@app.route("/")   #we created a host link that contains msg
-def greetings ():
-    return f"How are you doing?"
+@app.route("/")  #we created a host link that contains msg
+def index():
+    readme_file = open("README.md", "r")
+    md_template = markdown.markdown(readme_file.read(), extensions = ["fenced_code"])
+    return md_template
 
-@app.route("/random-number") #we created a route that displays a number
+@app.route("/random-number") #we created a special feature that generates a number between 0 and 1000
 def random_number():
-    return str(random.choice(range(0,11)))
-
-@app.route("/campus/<location>") #this would be another rout for location
-def campus_location(location):
-    if location == "BCN":
-        return "Carrer Pamplona 96"
-    elif location == "MAD":
-        return "Paseo de la Chopera, 14"
-
-@app.route("/line/<name>")
-def all_from_mongo(name):
-    lines = mongo.all_sentences(name)
-    return jsonify(lines)
-
+    return str(random.choice(range(0,1000)))
 
 if __name__ == '__main__': #this will check that the name is the meain
     app.run(debug=True) # we can define the port, port = 3000 and asignes the address
                 # debug= True/False, when you change something it updates
-
-
-test 23
